b4ca269b79383c931efc7128ea17b9bf6704a680
Eric Le <eric4leformal@gmail.com>
2025-03-10 14:11:21 -0500

    add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import CustomersTable from "@/app/ui/customers/table";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Customers() {
     return <p>Customers Page</p>;
     
